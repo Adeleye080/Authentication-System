0c30d4ff6fcdb39cec08b0ee3a294bb2ff374228
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-03-13 16:30:13 +0100

    validator to check if user exists

---
+++ api/utils/validators.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException
+
+
+def check_model_existence(db: Session, model, id):
+    """Checks if a model exists by its id"""
+
+    # obj = db.query(model).filter(model.id == id).first()
+    obj = db.get(model, ident=id)
+
+    if not obj:
+        raise HTTPException(status_code=404, detail=f"{model.__name__} does not exist")
+
+    return obj
