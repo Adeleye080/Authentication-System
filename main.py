f097f0dfd8808c4c10ca0f2bc1d5d95e950665dc
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-03-13 16:20:38 +0100

    response message and response model

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, Depends, status
+from fastapi import FastAPI, Depends, status, Request
 from contextlib import asynccontextmanager
 from api.v1.routes import api_version_one
+from api.utils.json_response import JsonResponseDict
+from api.v1.schemas.main import ProbeServerResponse, HomeResponse
 from api.utils.json_response import JsonResponseDict
 
 
@@ -19,21 +21,33 @@ app = FastAPI(
 )
 
 
-@app.get("/probe", tags=["Home"])
+@app.get(
+    "/probe",
+    tags=["Home"],
+    status_code=status.HTTP_200_OK,
+    response_model=ProbeServerResponse,
+)
 async def probe_server():
     """
     Probe the server
     """
-    return {"message": "I am the Python FastAPI API responding"}
+    return {
+        "message": "I am the Python FastAPI API responding to probe",
+        "status": "Healthy ♥️",
+    }
 
 
-@app.get("/", tags=["Home"])
+@app.get(
+    "/", tags=["Home"], status_code=status.HTTP_200_OK, response_model=HomeResponse
+)
 async def home():
     """
     Homepage
     """
     return JsonResponseDict(
-        message="Welcome to API", status_code=status.HTTP_200_OK, data={"URL": ""}
+        message="Welcome to Pius Python FastAPI Auth System",
+        status_code=status.HTTP_200_OK,
+        data={"URL": ""},
     )
 
 
