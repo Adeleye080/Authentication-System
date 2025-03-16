2dec30829cfd8ad4c88a218966aa3c7a17c9242d
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-03-16 17:43:53 +0100

    Homepage schema

--- api/v1/schemas/main.py
+++ api/v1/schemas/main.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import Dict
+from typing import Dict, Union
 
 
 class ProbeServerResponse(BaseModel):
@@ -12,11 +12,15 @@ class ProbeServerResponse(BaseModel):
         from_attributes = True
 
 
+class Author(BaseModel):
+    """Site Author"""
+    author: Dict[str, str]
+
 class HomeResponse(BaseModel):
     """
     Homepage Response Model
     """
 
     message: str
-    data: Dict
+    data: Union[Author, Dict[str, str]] = {"author": {}, "URL": "Site Url"}
     status_code: int = 200
