f2db743e34feb7d5a98c22402a2a82a103e82ff8
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-03-16 17:43:16 +0100

    url prefix

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -1,6 +1,13 @@
 from fastapi import APIRouter
 from api.v1.routes.user import user_router
+from api.v1.routes.admin_and_moderator_on_user import admin_and_moderator_on_user_router
+from api.v1.routes.admin_on_user import admin_on_user_router
+from api.v1.routes.auth import auth_router
 
-api_version_one = APIRouter(prefix="/api/v1")
 
+api_version_one = APIRouter(prefix="/api/v1/auth")
+
+api_version_one.include_router(auth_router)
 api_version_one.include_router(user_router)
+api_version_one.include_router(admin_and_moderator_on_user_router)
+api_version_one.include_router(admin_on_user_router)
