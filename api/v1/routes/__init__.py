06a56030ff16cd1b758f4756b40455a89f99a0a8
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-03-19 17:09:01 +0100

    register all existing routes

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter
 from api.v1.routes.user import user_router
-from api.v1.routes.admin_and_moderator_on_user import admin_and_moderator_on_user_router
-from api.v1.routes.admin_on_user import admin_on_user_router
 from api.v1.routes.auth import auth_router
+from api.v1.routes.two_factor_auth import two_factor_auth_router
+from api.v1.routes.audit_logs import audit_log_router
+from api.v1.routes.oauth import oauth_router
 
 
 api_version_one = APIRouter(prefix="/api/v1/auth")
 
 api_version_one.include_router(auth_router)
+api_version_one.include_router(oauth_router)
+api_version_one.include_router(two_factor_auth_router)
 api_version_one.include_router(user_router)
-api_version_one.include_router(admin_and_moderator_on_user_router)
-api_version_one.include_router(admin_on_user_router)
+api_version_one.include_router(audit_log_router)
