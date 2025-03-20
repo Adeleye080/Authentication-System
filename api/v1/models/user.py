505356cecf9ed7b2a0a6bd146d520e731bf4fbfb
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-03-20 16:18:08 +0100

    expanded user model

--- api/v1/models/user.py
+++ api/v1/models/user.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Column, Boolean, String, Index, Enum
+from sqlalchemy import Column, Boolean, String, Index, Enum, DateTime
 from sqlalchemy.orm import relationship
 from api.v1.models.base_model import BaseModel
-from api.v1.schemas.user import RoleEnum
+from api.v1.schemas.user import RoleEnum, LoginSource
 
 # call below line to ensure RefreshToken is found by mapper
 from api.v1.models.refresh_token import RefreshToken
+from api.v1.models.device import Device  # type: ignore
 
 
 class User(BaseModel):
@@ -17,7 +18,11 @@ class User(BaseModel):
     is_verified = Column(Boolean, default=False, nullable=False)
     is_deleted = Column(Boolean, default=False, nullable=False)
     role = Column(Enum(RoleEnum), default=RoleEnum.USER, nullable=False)
+    login_initiated = Column(Boolean, default=False, nullable=False)
+    last_login = Column(DateTime, nullable=True)
+    login_source = Column(Enum(LoginSource), nullable=True)
     refresh_tokens = relationship("RefreshToken", back_populates="user", uselist=True)
+    devices = relationship("UserDevice", backref="user", uselist=True)
 
     __table_args__ = (
         Index("ix_user_email", "email"),
@@ -26,6 +31,9 @@ class User(BaseModel):
         Index("ix_user_is_active", "is_active"),
         Index("ix_user_is_verified", "is_verified"),
         Index("ix_user_is_deleted", "is_deleted"),
+        Index("ix_user_login_initiated", "login_initiated"),
+        Index("ix_user_last_login", "last_login"),
+        Index("ix_user_login_source", "login_source"),
     )
 
     def to_dict(self):
@@ -36,3 +44,6 @@ class User(BaseModel):
 
     def __str__(self):
         return self.email
+
+    def __repr__(self):
+        return f"<User(id={self.id}, email={self.email}, role={self.role})>"
