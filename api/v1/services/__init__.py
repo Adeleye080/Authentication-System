c7da9ba374fbd331a423c04c701dae3e8ce6e502
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-03-24 23:45:10 +0100

    initialize audit log service

--- api/v1/services/__init__.py
+++ api/v1/services/__init__.py
@@ -1,6 +1,8 @@
 from api.v1.services.user import UserService
 from api.v1.services.device import DevicesService
+from api.v1.services.audit_log import AuditLogService
 
 
 user_service = UserService()
 devices_service = DevicesService()
+audit_log_service = AuditLogService()
