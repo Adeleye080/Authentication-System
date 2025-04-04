7d35f869a6fa89b3aee80e86c3992018029bf262
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-04-04 16:23:16 +0100

    http exception

--- api/utils/validators.py
+++ api/utils/validators.py
@@ -1,12 +1,18 @@
 from sqlalchemy.orm import Session
-from fastapi import HTTPException
+from fastapi import HTTPException, status
+from pydantic import EmailStr
 
 
-def check_model_existence(db: Session, model, id):
-    """Checks if a model exists by its id"""
+def check_model_existence(db: Session, model, id: str = None):
+    """Checks if a model exists by its id or any other optional attribute"""
 
-    # obj = db.query(model).filter(model.id == id).first()
-    obj = db.get(model, ident=id)
+    if id is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="No ID given"
+        )
+
+    if id:
+        obj = db.get(model, ident=id)
 
     if not obj:
         raise HTTPException(status_code=404, detail=f"{model.__name__} does not exist")
