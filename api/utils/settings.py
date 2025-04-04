223cde914aee95ecb1bad4c935cc4082b78fd350
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-04-04 16:24:45 +0100

    token and verification config

--- api/utils/settings.py
+++ api/utils/settings.py
@@ -16,7 +16,10 @@ class Settings(BaseSettings):
     COMPANY_ADDRESS: str = config(
         "COMPANY_ADDRESS", cast=str, default="123, auth system, texas, USA."
     )
-    COMPANY_LOGO_URL: str = config("COMPANY_LOGO_URL", default="http://#")
+    COMPANY_LOGO_URL: str = config(
+        "COMPANY_LOGO_URL",
+        default="https://ik.imagekit.io/flwjimnel/logo.png",
+    )
     COMPANY_TERMS_OF_SERVICE_URL: str = config(
         "COMPANY_TERMS_OF_SERVICE_URL", default="http://#"
     )
@@ -24,9 +27,13 @@ class Settings(BaseSettings):
         "COMPANY_PRIVACY_POLICY_URL", default="http://#"
     )
 
+    # APPLICATION SECRETS
     SECRET_KEY: str = config("SECRET_KEY")
     ALGORITHM: str = config("ALGORITHM")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = config("ACCESS_TOKEN_EXPIRE_MINUTES")
+    VERIFICATION_TOKEN_EXPIRATION_TIME: int = config(
+        "VERIFICATION_TOKEN_EXPIRATION_TIME", cast=int, default=600
+    )  # 10 minutes
     JWT_REFRESH_EXPIRY: int = config("JWT_REFRESH_EXPIRY")
 
     # Database configurations
@@ -43,15 +50,35 @@ class Settings(BaseSettings):
     SMTP_PORT: int = config("SMTP_PORT", cast=int, default=587)
     EMAIL_ADDRESS: str = config("EMAIL_ADDRESS")
     EMAIL_PASSWORD: str = config("EMAIL_PASSWORD")
+
     MJML_TEMPLATE_DIR: str = config(
-        "MJML_TEMPLATE_DIR", cast=str, default=f"{BASE_DIR}/mjml_mail_templates"
+        "MJML_TEMPLATE_DIR",
+        cast=str,
+        default=f"/home/ajiboye/Authentication-System/smtp/templates/html_mail_templates",  # points to the compiled html templates
     )
+
+    # MAIL GUN CONFIG
+    MAILGUN_API_KEY: str = config("MAILGUN_API_KEY")
+    MAILGUN_DOMAIN: str = config("MAILGUN_DOMAIN")
 
     # TWILIO_ACCOUNT_SID: str = config("TWILIO_ACCOUNT_SID")
     # TWILIO_AUTH_TOKEN: str = config("TWILIO_AUTH_TOKEN")
     # TWILIO_PHONE_NUMBER: str = config("TWILIO_PHONE_NUMBER")
 
-    # APP_NAME: str = config("APP_NAME")
+    # APP INFO
+    APP_NAME: str = config("APP_NAME", default="FASTAPI AUTH SYSTEM")
+    APP_URL: str = config("APP_URL", default="fastapi-authsystem.example.com")
+    FRONTEND_EMAIL_VERIFICATION_URL: str = config("FRONTEND_EMAIL_VERIFICATION_URL")
+    FRONTEND_MAGIC_LINK_VERIFICATION_URL: str = config(
+        "FRONTEND_MAGIC_LINK_VERIFICATION_URL"
+    )
+
+    ENCRYPTER_SECRET_KEY: str = config(
+        "ENCRYPTER_SECRET_KEY",
+        cast=str,
+        # generated using Fernet.generate_key()
+        default="Krq0Q8LWlYYv7famIjZ1k2gyzRZqEnKUqeEz2JX9CaQ=",
+    )
 
 
 settings = Settings()
