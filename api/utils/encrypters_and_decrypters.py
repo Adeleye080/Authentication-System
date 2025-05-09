06471c77f96e772ac1843d80f72dd67028846aef
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-05-09 18:15:37 +0100

    encrypter and decrypter for magiclink and user verification

--- api/utils/encrypters_and_decrypters.py
+++ api/utils/encrypters_and_decrypters.py
@@ -16,13 +16,13 @@ cipher_suite = Fernet(cipher_key)
 def encrypt_totp_secret(secret: str) -> str:
     """Encrypts a TOTP secret"""
 
-    return cipher_suite.encrypt(secret.encode()).decode()
+    return cipher_suite.encrypt(secret.encode())
 
 
 def decrypt_totp_secret(encrypted_secret: str) -> str:
     """Decrypts a TOTP secret"""
 
-    return cipher_suite.decrypt(encrypted_secret.encode()).decode()
+    return cipher_suite.decrypt(encrypted_secret)
 
 
 def generate_user_verification_token(user_email: EmailStr) -> str:
@@ -74,11 +74,12 @@ def decrypt_verification_token(token: str) -> EmailStr:
 
 def generate_magic_link_token(email: EmailStr, validity: int = 10):
     """
-    Generate magic link token for user login.
+    Generate magic link token for user passwordless login.\n
+    token format: email-validity-tokenType. \n
     Default validity is 10mins
     """
 
-    token_data = f"{email}-{(datetime.now(timezone.utc) + timedelta(minutes=validity)).timestamp()}"
+    token_data = f"{email}-{(datetime.now(timezone.utc) + timedelta(minutes=validity)).timestamp()}-magiclink"
 
     magic_link_token = base64.urlsafe_b64encode(
         cipher_suite.encrypt(token_data.encode())
@@ -95,10 +96,60 @@ def decrypt_magic_link_token(token: str) -> EmailStr:
     try:
         decoded_encrypted_data = base64.urlsafe_b64decode(token)
         decrypted_data = cipher_suite.decrypt(decoded_encrypted_data).decode()
-        email, expiry_timestamp = decrypted_data.split("-")
+        email, expiry_timestamp, token_type = decrypted_data.split("-")
 
         # Validate the email and expiry timestamp
-        print(email, expiry_timestamp)
+
+        assert token_type == "magiclink", "Invalid token type."
+        assert "@" in email, "Token is malformed."
+        assert expiry_timestamp is not None, "Token is missing important part"
+
+    except AssertionError as ase:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(ase))
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid Token"
+        )
+
+    current_timestamp = datetime.now(timezone.utc).timestamp()
+
+    if current_timestamp > float(expiry_timestamp):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Token has expired"
+        )
+
+    return email
+
+
+def generate_password_reset_token(email: EmailStr, validity: int = 30) -> str:
+    """
+    Generate and encrypt password reset token.\n
+    token format: email-validity-tokenType. \n
+    Default validity is 30mins
+    """
+
+    token_data = f"{email}-{(datetime.now(timezone.utc) + timedelta(minutes=validity)).timestamp()}-passwordreset"
+
+    password_reset_token = base64.urlsafe_b64encode(
+        cipher_suite.encrypt(token_data.encode())
+    ).decode()
+
+    return password_reset_token
+
+
+def decrypt_password_reset_token(token: str) -> EmailStr:
+    """
+    Decrypts and validates reset password token. Returns the user email address if valid.
+    """
+
+    try:
+        decoded_encrypted_data = base64.urlsafe_b64decode(token)
+        decrypted_data = cipher_suite.decrypt(decoded_encrypted_data).decode()
+        email, expiry_timestamp, token_type = decrypted_data.split("-")
+
+        # Validate the email and expiry timestamp
+
+        assert token_type == "passwordreset", "Invalid token type."
         assert "@" in email, "Token is malformed."
         assert expiry_timestamp is not None, "Token is missing important part"
 
