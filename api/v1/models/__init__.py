a03361520b9d76eacab0b5bc7038457a96b5956b
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-05-09 18:20:54 +0100

    initialize models

--- api/v1/models/__init__.py
+++ api/v1/models/__init__.py
@@ -1,4 +1,7 @@
 from api.v1.models.refresh_token import RefreshToken
 from api.v1.models.device import Device
+from api.v1.models.totp_device import TOTPDevice
 from api.v1.models.user import User
 from api.v1.models.audit_logs import AuditLog
+from api.v1.models.country_blacklist import CountryBlacklist, CountryBlacklistHistory
+from api.v1.models.attributes import UserAttribute
