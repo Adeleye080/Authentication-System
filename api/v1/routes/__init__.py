d376569ed6ca9c227f82fd2f8581884064f70e5c
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-05-09 18:18:03 +0100

    included user device router and country blacklisted router

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -4,6 +4,8 @@ from api.v1.routes.auth import auth_router
 from api.v1.routes.two_factor_auth import two_factor_auth_router
 from api.v1.routes.audit_logs import audit_log_router
 from api.v1.routes.oauth import oauth_router
+from api.v1.routes.country_blacklist import country_blacklist_router
+from api.v1.routes.device import user_device_router
 
 
 api_version_one = APIRouter(prefix="/api/v1/auth")
@@ -12,4 +14,6 @@ api_version_one.include_router(auth_router)
 api_version_one.include_router(oauth_router)
 api_version_one.include_router(two_factor_auth_router)
 api_version_one.include_router(user_router)
+api_version_one.include_router(user_device_router)
 api_version_one.include_router(audit_log_router)
+api_version_one.include_router(country_blacklist_router)
