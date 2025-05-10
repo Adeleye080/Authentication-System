f9ea94fe553b650dd8cc88ebf618dc3606872e43
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-05-10 12:30:12 +0100

    Add TOTPService initialization to services module

--- api/v1/services/__init__.py
+++ api/v1/services/__init__.py
@@ -2,8 +2,10 @@ from api.v1.services.user import UserService
 from api.v1.services.device import DevicesService
 from api.v1.services.audit_log import AuditLogService
 from api.v1.services.notification import Notification
+from api.v1.services.totp import TOTPService
 
 user_service = UserService()
 devices_service = DevicesService()
 audit_log_service = AuditLogService()
 notification_service = Notification()
+totp_service = TOTPService()
