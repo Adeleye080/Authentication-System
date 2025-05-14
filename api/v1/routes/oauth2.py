522db7c552a82d933970c5fbcc1e2fd0256d9851
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-05-14 21:53:45 +0100

    Enhance OAuth2 login flow with user creation, token generation, and audit logging

--- api/v1/routes/oauth2.py
+++ api/v1/routes/oauth2.py
@@ -1,94 +1,173 @@
-from fastapi import APIRouter, Depends, HTTPException
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks, status
+from fastapi.responses import RedirectResponse
 from authlib.integrations.starlette_client import OAuth  # type: ignore
 from starlette.requests import Request
-from starlette.config import Config
-from api.utils.settings import settings
-import jwt
-import datetime
 import logging
-from api.utils.json_response import JsonResponseDict
-from api.v1.services import oauth2_service
+from api.utils.settings import settings
+from api.v1.services import oauth2_service, user_service, audit_log_service
+from api.v1.models.user import User
+from api.v1.schemas.audit_logs import (
+    AuditLogCreate,
+    AuditLogEventEnum,
+    AuditLogStatuses,
+)
+from api.v1.schemas.user import LoginSource
+from sqlalchemy.orm import Session
+from db.database import get_db
+import random
+import datetime as dt
 
-logging.basicConfig(level=logging.INFO)
+
 logger = logging.getLogger(__name__)
 
-# Initialize FastAPI app
+
 oauth2_router = APIRouter(prefix="/oauth2", tags=["OAuth2"])
 
-# OAuth configurations
-config = Config(environ={})
-oauth = OAuth(config)
 
-oauth.register(
-    name="github",
-    client_id=settings.GITHUB_CLIENT_ID,
-    client_secret=settings.GITHUB_CLIENT_SECRET,
-    access_token_url="https://github.com/login/oauth/access_token",
-    authorize_url="https://github.com/login/oauth/authorize",
-    api_base_url="https://api.github.com/",
-    client_kwargs={"scope": "user:email"},
-)
-
-oauth.register(
-    name="google",
-    client_id=settings.GOOGLE_CLIENT_ID,
-    client_secret=settings.GOOGLE_CLIENT_SECRET,
-    access_token_url="https://oauth2.googleapis.com/token",
-    authorize_url="https://accounts.google.com/o/oauth2/auth",
-    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
-    api_base_url="https://www.googleapis.com/oauth2/v1/",
-    client_kwargs={"scope": "openid email profile"},
-)
-
-oauth.register(
-    name="facebook",
-    client_id=settings.FACEBOOK_CLIENT_ID,
-    client_secret=settings.FACEBOOK_CLIENT_SECRET,
-    access_token_url="https://graph.facebook.com/oauth/access_token",
-    authorize_url="https://www.facebook.com/dialog/oauth",
-    api_base_url="https://graph.facebook.com/",
-    client_kwargs={"scope": "email"},
-)
-
-
-@oauth2_router.get("/login/{provider}")
+@oauth2_router.post("/login/{provider}")
 async def login(request: Request, provider: str):
     """Login route for OAuth2 providers"""
 
-    if provider not in oauth._registry:
+    if provider not in oauth2_service.secureOAuth()._registry:
         raise HTTPException(status_code=400, detail="Unsupported provider")
 
     # Redirect to the provider's authorization URL
     if provider == "github":
         redirect_uri = request.url_for("authorize", provider="github")
-        return await oauth.github.authorize_redirect(request, redirect_uri)
+        return await oauth2_service.secureOAuth().github.authorize_redirect(
+            request, redirect_uri
+        )
     elif provider == "facebook":
         redirect_uri = request.url_for("authorize", provider="facebook")
-        return await oauth.facebook.authorize_redirect(request, redirect_uri)
+        return await oauth2_service.secureOAuth().facebook.authorize_redirect(
+            request, redirect_uri
+        )
     elif provider == "google":
         redirect_uri = request.url_for("authorize", provider="google")
-        return await oauth.google.authorize_redirect(request, redirect_uri)
+        return await oauth2_service.secureOAuth().google.authorize_redirect(
+            request, redirect_uri
+        )
 
 
 @oauth2_router.get("/authorize/{provider}", include_in_schema=False)
-async def authorize(provider: str, request: Request):
+async def authorize(
+    provider: str, request: Request, bgt: BackgroundTasks, db: Session = Depends(get_db)
+):
     """Authorization callback route for OAuth2 providers"""
 
-    if provider not in oauth._registry:
+    if provider not in oauth2_service.secureOAuth()._registry:
         raise HTTPException(status_code=400, detail="Unsupported provider")
 
-    token = await oauth.google.authorize_access_token(request)
     if provider == "github":
-        user_info = await oauth.github.get("user", token=token)
+        token = await oauth2_service.secureOAuth().github.authorize_access_token(
+            request
+        )
+        user_info = await oauth2_service.secureOAuth().github.get("user", token=token)
     elif provider == "google":
-        user_info = await oauth.google.get("userinfo", token=token)
+        token = await oauth2_service.secureOAuth().google.authorize_access_token(
+            request
+        )
+        user_info = await oauth2_service.secureOAuth().google.get(
+            "userinfo", token=token
+        )
     elif provider == "facebook":
-        user_info = await oauth.facebook.get("me?fields=id,name,email", token=token)
+        token = await oauth2_service.secureOAuth().facebook.authorize_access_token(
+            request
+        )
+        user_info = await oauth2_service.secureOAuth().facebook.get(
+            "me?fields=id,name,email,first_name,middle_name,last_name,birthday,gender,picture",
+            token=token,
+        )
     else:
         raise HTTPException(status_code=400, detail="Unsupported provider")
 
-    user_info = user_info.json()
-    print("\n\n", user_info, "\n\n")
-    jwt_token = {"token": "some-secure-token"}
-    return JSONResponse(content={"token": jwt_token, "user_info": user_info})
+    user_email = user_info.get("email", None)
+
+    if not user_email:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Invalid user object from OAuth provider",
+        )
+
+    # generate user access and refresh tokens
+    user_exist, user_obj = User().user_exists(db=db, email=user_email)
+
+    if not user_exist:
+        user = User(email=user_email, password=str(random.randint(5, 15)))
+        user.save(db=db)
+
+        # send new user info to webhook url in the background
+        oauth2_service.post_oauth_signup_webhook(bgt, user_info)
+    else:
+        user = user_obj
+
+    user_access_token = user_service.create_access_token(db=db, user_obj=user)
+    user_refresh_token = user_service.create_refresh_token(db=db, user_id=user.id)
+    user.last_login = dt.datetime.now(dt.timezone.utc)
+
+    if provider == "github":
+        user.login_source = LoginSource.GITHUB
+    elif provider == "facebook":
+        user.login_source = LoginSource.FACEBOOK
+    elif provider == "google":
+        user.login_source = LoginSource.GOOGLE
+
+    # update login source and last login time
+    user.save(db=db)
+
+    # construct redirect url
+    # add access and refresh tokens to the redirect uri if cookies are not allowed
+    # otherwise, set them in the cookies
+    redirect_uri = (
+        f"{settings.FRONTEND_HOME_URL.strip('/')}/auth-callback?auth_success=true"
+    )
+    if not settings.ALLOW_AUTH_COOKIES:
+        redirect_uri += (
+            f"&access_token={user_access_token}&refresh_token={user_refresh_token}"
+        )
+
+    # redirect to frontend with success message
+    response = RedirectResponse(
+        url=redirect_uri,
+        status_code=status.HTTP_303_SEE_OTHER,
+    )
+
+    # set cookies
+    if settings.ALLOW_AUTH_COOKIES:
+        response.set_cookie(
+            key="access_token",
+            value=user_access_token,
+            httponly=True,
+            secure=settings.AUTH_SECURE_COOKIES,
+            samesite=settings.AUTH_SAME_SITE,
+            expires=settings.ACCESS_TOKEN_EXPIRE_MINUTES,
+        )
+
+        response.set_cookie(
+            key="refresh_token",
+            value=user_refresh_token,
+            httponly=True,
+            secure=settings.AUTH_SECURE_COOKIES,
+            samesite=settings.AUTH_SAME_SITE,
+            expires=settings.JWT_REFRESH_EXPIRY,
+        )
+
+    # audit log
+    audit_log_service.log(
+        db=db,
+        background_task=bgt,
+        schema=AuditLogCreate(
+            user_id=user.id,
+            event=AuditLogEventEnum.LOGIN,
+            status=AuditLogStatuses.SUCCESS,
+            ip_address="Not Available",
+            details={
+                "provider": provider,
+                "user_info": user_info,
+            },
+            user_agent="Not Available",
+            description=f"OAuth Login: User {user.email} logged in using {provider}",
+        ),
+    )
+
+    return response
