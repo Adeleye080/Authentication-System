d6a44bfdae1d3f388dd6e3395dadc7f8170b56bf
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-05-14 20:00:24 +0100

    Add OAuth2Service initialization to service module

--- api/v1/services/__init__.py
+++ api/v1/services/__init__.py
@@ -3,9 +3,11 @@ from api.v1.services.device import DevicesService
 from api.v1.services.audit_log import AuditLogService
 from api.v1.services.notification import Notification
 from api.v1.services.totp import TOTPService
+from api.v1.services.oauth2 import OAuth2Service
 
 user_service = UserService()
 devices_service = DevicesService()
 audit_log_service = AuditLogService()
 notification_service = Notification()
 totp_service = TOTPService()
+oauth2_service = OAuth2Service()
