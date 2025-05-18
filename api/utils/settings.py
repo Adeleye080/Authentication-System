732f90ebc75724b40f593f23853d8a85c8addd7f
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-05-18 21:46:25 +0100

    Update AUTH_SAME_SITE default value to 'strict' for improved security

--- api/utils/settings.py
+++ api/utils/settings.py
@@ -44,7 +44,7 @@ class Settings(BaseSettings):
     )  # 30 days
     ALLOW_AUTH_COOKIES: bool = config("ALLOW_AUTH_COOKIES", cast=bool, default=True)
     AUTH_SECURE_COOKIES: bool = config("SECURE_COOKIES", cast=bool, default=True)
-    AUTH_SAME_SITE: str = config("SAME_SITE", cast=str, default="None")
+    AUTH_SAME_SITE: str = config("SAME_SITE", cast=str, default="strict")
 
     # DATABASE CONFIG
     DB_HOST: str = config("DB_HOST")
