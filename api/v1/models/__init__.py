51531659ed5db8c7e8f48bd3144ef33fac23b545
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-06-06 18:56:30 +0100

    removed mmdb tracker

--- api/v1/models/__init__.py
+++ api/v1/models/__init__.py
@@ -6,6 +6,3 @@ from api.v1.models.audit_logs import AuditLog
 from api.v1.models.country_blacklist import CountryBlacklist, CountryBlacklistHistory
 from api.v1.models.attributes import UserAttribute
 from api.v1.models.mmdb import MMDB_TRACKER
-
-
-MMDB_TRACKER()
