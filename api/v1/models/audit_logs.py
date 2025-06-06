9c473430e37d4c77f44afe5949e1b858d1ea96a2
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-06-06 18:57:44 +0100

    event column

--- api/v1/models/audit_logs.py
+++ api/v1/models/audit_logs.py
@@ -1,8 +1,6 @@
-from sqlalchemy import Column, String, Text, JSON, DateTime, func, Index, Enum, Integer
+from sqlalchemy import Column, String, Text, JSON, DateTime, func, Index, Integer
 from db.database import Base
 from fastapi import Depends
-from uuid_extensions import uuid7
-from api.v1.schemas.audit_logs import AuditLogEventEnum
 
 
 class AuditLog(Base):
@@ -12,7 +10,7 @@ class AuditLog(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(String(36), nullable=False)
-    event = Column(Enum(AuditLogEventEnum), nullable=False)
+    event = Column(String(100), nullable=False)
     status = Column(String(50), nullable=False)
     description = Column(Text, nullable=False)
     timestamp = Column(
