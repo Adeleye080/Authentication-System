a21a9aa9868ff1e9dc7101739732b4d87596707c
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-06-12 22:36:58 +0100

    feat: enhance fetch_all_audit_logs endpoint with pagination and filtering options

--- api/v1/routes/audit_logs.py
+++ api/v1/routes/audit_logs.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, status, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 from typing import Optional
 from datetime import datetime
-from api.v1.schemas.audit_logs import AuditLogEventEnum
+from api.v1.schemas.audit_logs import AllLogsResponse
+from api.utils.responses import all_logs_response
 from db.database import get_db
 from api.v1.services import user_service, audit_log_service
 from api.v1.models.user import User
@@ -12,22 +13,57 @@ audit_log_router = APIRouter(prefix="/logs", tags=["Audit Logs"])
 
 
 @audit_log_router.get(
-    "/",
+    "",
     summary="Fetch all audit logs",
     status_code=status.HTTP_200_OK,
+    response_model=AllLogsResponse,
 )
 def fetch_all_audit_logs(
     event: Optional[str] = Query(None),
     status: Optional[str] = Query(None),
     start_time: Optional[datetime] = Query(None),
     end_time: Optional[datetime] = Query(None),
+    page: int = 1,
+    per_page: int = 100,
     db: Session = Depends(get_db),
     moderator_superadmin: User = Depends(user_service.get_current_user),
 ):
     """Fetches all audit logs"""
 
-    audit_log_service.get
-    pass
+    page = max(page, 1)
+    per_page = max(per_page, 1)
+    if per_page > 100:
+        per_page = 100
+
+    if event:
+        event = event.upper()
+    if status:
+        status = status.upper()
+
+    logs, total_logs = audit_log_service.fetch_logs_with_filters_and_pagination(
+        db=db,
+        event=event,
+        status=status,
+        start_time=start_time,
+        end_time=end_time,
+        page=page,
+        limit=per_page,
+    )
+
+    logs_count = len(logs)
+    total_pages = (total_logs + per_page - 1) // per_page
+
+    return all_logs_response(
+        current_page=page,
+        per_page=per_page,
+        total=total_logs,
+        total_pages=total_pages,
+        count=logs_count,
+        status_code=200,
+        data=[log.to_dict() for log in logs],
+        prev_page=f"/?page={page - 1}" if page > 1 else None,
+        next_page=f"/?page={page + 1}" if page < total_pages else None,
+    )
 
 
 @audit_log_router.get(
