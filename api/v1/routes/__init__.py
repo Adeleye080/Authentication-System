f2e4211ae117d8acc9a1b6abc2a647d6d67384b6
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-06-18 19:20:42 +0100

    fix: replace user_router with account_router in API routes

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from api.v1.routes.user import user_router
+from api.v1.routes.user import account_router
 from api.v1.routes.auth import auth_router
 from api.v1.routes.two_factor_auth import two_factor_auth_router
 from api.v1.routes.audit_logs import audit_log_router
@@ -7,7 +7,6 @@ from api.v1.routes.oauth2 import oauth2_router
 from api.v1.routes.country_blacklist import country_blacklist_router
 from api.v1.routes.device import user_device_router
 from api.v1.routes.app_service import app_router
-from api.v1.routes.account import account_router
 
 
 api_version_one = APIRouter(prefix="/auth/api/v1")
@@ -15,7 +14,6 @@ api_version_one = APIRouter(prefix="/auth/api/v1")
 api_version_one.include_router(auth_router)
 api_version_one.include_router(oauth2_router)
 api_version_one.include_router(two_factor_auth_router)
-api_version_one.include_router(user_router)
 api_version_one.include_router(account_router)
 api_version_one.include_router(user_device_router)
 api_version_one.include_router(audit_log_router)
