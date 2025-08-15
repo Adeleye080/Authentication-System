33368620c9fe2b0e6ddb07f7f0d6c3d0f1c000eb
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-08-15 12:50:44 +0100

    fix: correct ordinal suffix logic and update date format in normalize_date function

--- api/utils/dates.py
+++ api/utils/dates.py
@@ -6,7 +6,7 @@ def add_ordinal_suffix(day) -> str:
     if 10 <= day <= 20:
         suffix = "th"
     else:
-        suffix = {1: "str", 2: "nd", 3: "rd"}.get(day % 10, "th")
+        suffix = {1: "st", 2: "nd", 3: "rd"}.get(day % 10, "th")
 
     return f"{day}{suffix}"
 
@@ -14,11 +14,13 @@ def add_ordinal_suffix(day) -> str:
 def normalize_date(date: datetime) -> str:
     """
     Convert datetime to normal date string.\n
-    e.g '20th January 2025'
+    e.g '20th January 2025 by 12:30 PM'
     """
 
     day = date.day
     month = date.strftime("%B")
     year = date.year
 
-    return f"{add_ordinal_suffix(day)} {month} {year}"
+    time = date.time().strftime("%H:%M %p")
+
+    return f"{add_ordinal_suffix(day)} {month} {year} by {time}"
