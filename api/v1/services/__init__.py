2f844333009a95707d49c94678cf01dc23341da4
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-08-30 20:18:42 +0100

    register  roles and webhook services

--- api/v1/services/__init__.py
+++ api/v1/services/__init__.py
@@ -7,6 +7,8 @@ from api.v1.services.oauth2 import OAuth2Service
 from api.v1.services.geoip import GeoIPService
 from api.v1.services.country_blacklists import CountryBlacklistService
 from api.v1.services.app_services import ApplicationService
+from api.v1.services.role_service import RoleService
+from api.v1.services.webhook import WebhookService
 
 user_service = UserService()
 devices_service = DevicesService()
@@ -17,3 +19,5 @@ oauth2_service = OAuth2Service()
 geoip_service = GeoIPService()
 country_blacklist_service = CountryBlacklistService()
 application_service = ApplicationService()
+secondary_role_service = RoleService()
+webhook_service = WebhookService()
