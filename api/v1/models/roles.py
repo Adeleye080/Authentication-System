d73a99a3a8f601c7ba388b30342e5f47190cdc27
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-08-30 20:18:11 +0100

    method to convert obj to dict

--- api/v1/models/roles.py
+++ api/v1/models/roles.py
@@ -26,3 +26,30 @@ class SecondaryRole(Base):
     created_by = Column(String(36), nullable=False)
 
     __table_args__ = (Index("user_secondary_role", "name"),)
+
+    def to_dict(
+        self, hide_creator: bool = False, hide_desc: bool = False, hide_id: bool = False
+    ) -> dict:
+        """
+        Convert object to dictionary
+
+        Args:
+        :param hide_creator: hide the creator id
+        :param hide_desc: hide the role description
+        :param hide_id: hide the role id
+        """
+
+        obj_dict = self.__dict__.copy()
+        if obj_dict["_sa_instance_state"]:
+            del obj_dict["_sa_instance_state"]
+        if self.created_at:
+            obj_dict["created_at"] = self.created_at.isoformat()
+
+        # Hide options
+        if hide_creator:
+            del obj_dict["created_by"]
+        if hide_desc:
+            del obj_dict["description"]
+        if hide_id:
+            del obj_dict["id"]
+        return obj_dict
