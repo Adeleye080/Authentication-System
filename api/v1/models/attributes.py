4559285e81ccac14363a2469f3a474747d319a7a
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-09-03 20:48:09 +0100

    predefined attribute values relationship

--- api/v1/models/attributes.py
+++ api/v1/models/attributes.py
@@ -1,15 +1,41 @@
-from api.v1.models.base_model import BaseModel
-from sqlalchemy import Column, String, ForeignKey, Index
+from api.v1.models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Index
+from sqlalchemy.orm import relationship
+
+
+class AttributeValues(Base):
+    __tablename__ = "auth_predefined_attr_vals"
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    attribute_id = Column(
+        Integer, ForeignKey("auth_user_attributes.id"), nullable=False
+    )
+    value = Column(String(100), nullable=False)
+
+
+class Attributes(Base):
+    __tablename__ = "auth_user_attributes"
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(60), nullable=False, unique=True)
+
+    allowed_values = relationship(
+        AttributeValues, backref="attribute", cascade="all, delete-orphan", uselist=True
+    )
+
+    __table_args__ = (Index("ix_auth_user_attributes_name", "name", unique=True),)
 
 
 class UserAttribute(BaseModel):
-    __tablename__ = "auth_user_attributes"
+    __tablename__ = "auth_user_attrs_association"
 
-    user_id = Column(ForeignKey("auth_users.id"), nullable=False)
-    key = Column(String(128), nullable=False)
-    value = Column(String(256), nullable=False)
-
-    __table_args__ = (
-        Index("ix_auth_user_attribute_key", "key"),
-        Index("ix_auth_user_attribute_value", "value"),
+    user_id = Column(ForeignKey("auth_users.id"), nullable=False, primary_key=True)
+    attribute_id = Column(
+        Integer, ForeignKey("auth_user_attributes.id"), nullable=False, primary_key=True
     )
+    attribute_value_id = Column(
+        ForeignKey("auth_predefined_attr_vals.id"), nullable=False
+    )
+
+    attribute = relationship("Attributes", backref="user_attributes_link")
+    attribute_value = relationship("AttributeValues", backref="user_attributes_link")
